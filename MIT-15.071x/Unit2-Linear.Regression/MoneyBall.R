0c702eb74baee5f1fbcb5d57005abf18036c3265
Chanseok Kang <kcsgoodboy@gmail.com>
2020-03-25 22:53:50 +0900

    Added Lecture Example
    Added Lecture Example for analyzing moneyball

---
+++ MIT-15.071x/Unit2-Linear.Regression/MoneyBall.R
@@ -0,0 +1,20 @@
+# Read Data
+baseball = read.csv('./dataset/baseball.csv')
+
+# Structure of Data
+str(baseball)
+
+# Subset the data
+moneyball = subset(baseball, Year < 2002)
+str(moneyball)
+
+# Create new variable to show difference between RS and RA
+moneyball$RD = moneyball$RS - moneyball$RA
+str(moneyball)
+
+# Visualize linear relationship
+plot(moneyball$RD, moneyball$W)
+
+# Create lm model
+WinsReg = lm(W ~ RD, data=moneyball)
+summary(WinsReg)
