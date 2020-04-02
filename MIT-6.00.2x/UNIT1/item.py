a809b820bb64c6c17df4b9444bc20dae44af9bba
chanseok <chanseok.kang@lge.com>
2020-04-02 17:30:18 +0900

    type: Add & fix
    Add lec2 exercise1 & renamed folder name

---
+++ MIT-6.00.2x/UNIT1/item.py
@@ -0,0 +1,28 @@
+import random
+
+class Item(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = float(v)
+        self.weight = float(w)
+    def getName(self):
+        return self.name
+    def getValue(self):
+        return self.value
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return '<' + self.name + ', ' + str(self.value) + ', '\
+                     + str(self.weight) + '>'
+    
+def buildItems():
+    return [Item(n,v,w) for n,v,w in (('clock', 175, 10),
+                                      ('painting', 90, 9),
+                                      ('radio', 20, 4),
+                                      ('vase', 50, 2),
+                                      ('book', 10, 1),
+                                      ('computer', 200, 20))]
+
+def buildRandomItems(n):
+    return [Item(str(i),10*random.randint(1,10),random.randint(1,10))
+            for i in range(n)]
