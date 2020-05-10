63c5f70cb58efed57fa988e4c7b5957c8375ed4c
Chanseok Kang <kcsgoodboy@gmail.com>
2020-05-10 23:32:51 +0900

    type: update
    Update lecture 10 notebook

--- MIT-6.00.2x/UNIT4/lecture10_segment3.py
+++ MIT-6.00.2x/UNIT4/lecture10_segment3.py
@@ -5,7 +5,7 @@ Created on Mon Sep 19 11:45:20 2016
 @author: johnguttag
 """
 
-import random, pylab, numpy
+import pylab, numpy
 
 #set line width
 pylab.rcParams['lines.linewidth'] = 4
@@ -61,14 +61,14 @@ def fitData(fileName):
                label = 'Measured points')
     labelPlot()                 
     a,b = pylab.polyfit(xVals, yVals, 1)
-    estYVals = a*xVals + b
+    estYVals = a*pylab.array(xVals) + b
     print('a =', a, 'b =', b)
     pylab.plot(xVals, estYVals, 'r',
                label = 'Linear fit, k = '
                + str(round(1/a, 5)))
     pylab.legend(loc = 'best')
     
-# fitData('springData.txt')
+#fitData('springData.txt')
 
    
 def fitData1(fileName):
@@ -88,26 +88,7 @@ def fitData1(fileName):
 
 #fitData1('springData.txt')
 
-def genParabolicData(a, b, c, xVals, fracOutliers):
-   yVals = []
-   for x in xVals:
-       theoreticalVal = a*x**2 + b*x + c
-       if random.random() > fracOutliers:
-           yVals.append(theoreticalVal\
-           + random.gauss(0, 1000))
-       else: #generate outlier
-           yVals.append(theoreticalVal\
-           + random.gauss(0, theoreticalVal*2))
-   return yVals
-#    
-##parameters for generating data
-#xVals = range(-50, 51, 5)
-#a, b, c = 3.0, 0.0, 0.0
-#fracOutlier = 0.00
-#
-##generate data
-#random.seed(0)
-#yVals = genParabolicData(a, b, c, xVals, fracOutlier)
+#xVals, yVals = getData('mysteryData.txt')
 #pylab.plot(xVals, yVals, 'o', label = 'Data Points')
 #pylab.title('Mystery Data')
 #
@@ -121,5 +102,82 @@ def genParabolicData(a, b, c, xVals, fracOutliers):
 #pylab.plot(xVals, pylab.polyval(model2, xVals),
 #           'r--', label = 'Quadratic Model')
 #pylab.legend()
+#
+##Compare models
+#def aveMeanSquareError(data, predicted):
+#    error = 0.0
+#    for i in range(len(data)):
+#        error += (data[i] - predicted[i])**2
+#    return error/len(data)
 
+#estYVals = pylab.polyval(model1, xVals)  
+#print('Ave. mean square error for linear model =',
+#      aveMeanSquareError(yVals, estYVals))
+#estYVals = pylab.polyval(model2, xVals)
+#print('Ave. mean square error for quadratic model =',
+#      aveMeanSquareError(yVals, estYVals))
+
+def rSquared(observed, predicted):
+    error = ((predicted - observed)**2).sum()
+    meanError = error/len(observed)
+    return 1 - (meanError/numpy.var(observed))
+
+def genFits(xVals, yVals, degrees):
+    models = []
+    for d in degrees:
+        model = pylab.polyfit(xVals, yVals, d)
+        models.append(model)
+    return models
+
+def testFits(models, degrees, xVals, yVals, title):
+    pylab.plot(xVals, yVals, 'o', label = 'Data')
+    for i in range(len(models)):
+        estYVals = pylab.polyval(models[i], xVals)
+        error = rSquared(yVals, estYVals)
+        pylab.plot(xVals, estYVals,
+                   label = 'Fit of degree '\
+                   + str(degrees[i])\
+                   + ', R2 = ' + str(round(error, 5)))
+    pylab.legend(loc = 'best')
+    pylab.title(title)
+
+# xVals, yVals = getData('mysteryData.txt')
+#degrees = (1, 2)
+#models = genFits(xVals, yVals, degrees)
+#testFits(models, degrees, xVals, yVals, 'Mystery Data')
+
+#Compare higher-order fits
+# degrees = (2, 4, 8, 16)
+# models = genFits(xVals, yVals, degrees)
+# testFits(models, degrees, xVals, yVals, 'Mystery Data')
+
+
+
+
+
+#def genParabolicData(a, b, c, xVals, fracOutliers):
+#    yVals = []
+#    for x in xVals:
+#        theoreticalVal = a*x**2 + b*x + c
+#        if random.random() > fracOutliers:
+#            yVals.append(theoreticalVal\
+#            + random.gauss(0, 35))
+#        else: #generate outlier
+#            yVals.append(theoreticalVal\
+#            + random.gauss(0, theoreticalVal*2))
+#    f = open('mystery.txt','w')
+#    f.write('x        y\n')
+#    for i in range(len(yVals)):
+#        f.write(str(yVals[i]) + ' ' + str(xVals[i]) + '\n')
+#    f.close()
+#    return yVals
+#    
+##parameters for generating data
+#xVals = range(-10, 11, 1)
+#a, b, c = 3.0, 0.0, 0.0
+#fracOutlier = 0.00
+#
+##generate data
+#random.seed(0)
+#yVals = genParabolicData(a, b, c, xVals, fracOutlier)
 
